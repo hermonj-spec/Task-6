bbfded99e9d915b397d75b44f53fee108450aed8
hermonj-spec <hermon.samsonj@gmail.com>
2025-10-17 15:08:57 +0300

    Commit 1
    A new LaptopUtils with a static method to use a Laptop object polymorphically by calling its performAction method was added to the repository.

---
+++ Task6/src/LaptopUtils.java
@@ -0,0 +1,6 @@
+public class LaptopUtils {
+    public static void useLaptop(Laptop laptop) {
+        System.out.println("Using laptop polymorphically:");
+        laptop.performAction();
+    }
+}
