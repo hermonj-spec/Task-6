80a5cb2867c58d32eedc8f4539186dd5e0f422e3
hermonj-spec <hermon.samsonj@gmail.com>
2025-10-17 15:40:25 +0300

    Commit 1
    A new Main class to show overriding, overloading, and polymorphism using GamingLaptop and BusinessLaptop classes, and LaptopUtils utility methods was added to the repository.

---
+++ Task6/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        GamingLaptop GL = new GamingLaptop();
+        BusinessLaptop BL = new BusinessLaptop();
+
+        System.out.println("-------Overriding-------");
+        GL.performAction();
+        BL.performAction();
+
+        System.out.println("\n-------Overloading-------");
+        GL.displayDetails();
+        BL.displayDetails("Lightweight, 8GB RAM, Optimized for business task");
+
+        System.out.println("\n-------Polymorphism-------");
+        LaptopUtils.useLaptop(GL);
+        LaptopUtils.useLaptop(BL);
+    }
+}
