dc0b55a9b4042477e5b06a29f73c1018ccab1afc
hermonj-spec <hermon.samsonj@gmail.com>
2025-10-17 14:33:07 +0300

    Commit 2
    In the new version an overload method for custom information was added.

--- Task6/src/Laptop.java
+++ Task6/src/Laptop.java
@@ -6,4 +6,8 @@ public class Laptop {
     public void displayDetails() {
         System.out.println("Laptop Details: No additional information provided");
     }
+
+    public void displayDetails(String info) {
+        System.out.println("Laptop Details: " + info);
+    }
 }
