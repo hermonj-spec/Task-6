efc9dc8b1222fc10fa746a348b34d64a9f468595
hermonj-spec <hermon.samsonj@gmail.com>
2025-10-17 14:31:51 +0300

    Commit 1
    A new Laptop class with methods to  performAction and displayDetails, including an overloaded method for basic functionality and output was added to the repository.

---
+++ Task6/src/Laptop.java
@@ -0,0 +1,9 @@
+public class Laptop {
+    public void performAction() {
+        System.out.println("Laptop is booting on.");
+    }
+
+    public void displayDetails() {
+        System.out.println("Laptop Details: No additional information provided");
+    }
+}
