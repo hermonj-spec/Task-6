e2465bc67acd45713c9febb798fcd1e6b0d9cc84
hermonj-spec <hermon.samsonj@gmail.com>
2025-10-17 14:37:57 +0300

    Commit 2
    In the new version on line 4, an enhance to the output message in performAction method to specify launching a high performance game with max-graphics was added.

--- Task6/src/GamingLaptop.java
+++ Task6/src/GamingLaptop.java
@@ -1,6 +1,6 @@
 public class GamingLaptop extends Laptop {
     @Override
     public void performAction() {
-        System.out.println("Gaming Laptop is launching.");
+        System.out.println("Gaming Laptop is launching a high performance game with max-graphics.");
     }
 }
