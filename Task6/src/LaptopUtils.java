deb7089d8cab4628b7b73890c647b900ec75b992
hermonj-spec <hermon.samsonj@gmail.com>
2025-10-17 15:11:09 +0300

    Commit 2
    In the new version on line 5, a separator line is printed after performing the laptop action to improve console output readability was added to the repository.

--- Task6/src/LaptopUtils.java
+++ Task6/src/LaptopUtils.java
@@ -2,5 +2,6 @@ public class LaptopUtils {
     public static void useLaptop(Laptop laptop) {
         System.out.println("Using laptop polymorphically:");
         laptop.performAction();
+        System.out.println("-----------------------------");
     }
 }
