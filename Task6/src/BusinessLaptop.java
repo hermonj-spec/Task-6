c73f41a66788ecfd5629b4d899590e782ffbcbea
hermonj-spec <hermon.samsonj@gmail.com>
2025-10-17 14:50:15 +0300

    Commit 2
    In the new version on line 4, Changed the output of performAction to specify that the business laptop opens a secure VPN and production apps, providing more context for its specialized behavior.

--- Task6/src/BusinessLaptop.java
+++ Task6/src/BusinessLaptop.java
@@ -1,6 +1,6 @@
 public class BusinessLaptop extends Laptop {
     @Override
     public void performAction() {
-        System.out.println("Business Laptop is opening.");
+        System.out.println("Business Laptop is opening a secure VPN and production apps.");
     }
 }
